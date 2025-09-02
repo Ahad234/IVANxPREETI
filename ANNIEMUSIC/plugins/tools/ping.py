fc28c0f92dd114dec722f21691b9fb94f46ccff9
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-03 00:38:58 +0530

    Update ping.py

--- ANNIEMUSIC/plugins/tools/ping.py
+++ ANNIEMUSIC/plugins/tools/ping.py
@@ -15,14 +15,31 @@ from config import BANNED_USERS, PING_VID_URL
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_video(
-        video=PING_VID_URL,
-        caption=_["ping_1"].format(app.mention),
-    )
+
+    # ✅ Flexible handling for image/video/text
+    if PING_VID_URL and PING_VID_URL.lower().endswith((".mp4", ".mkv")):
+        response = await message.reply_video(
+            video=PING_VID_URL,
+            caption=_["ping_1"].format(app.mention),
+        )
+        edit_func = response.edit_caption
+    elif PING_VID_URL and PING_VID_URL.lower().endswith((".png", ".jpg", ".jpeg")):
+        response = await message.reply_photo(
+            photo=PING_VID_URL,
+            caption=_["ping_1"].format(app.mention),
+        )
+        edit_func = response.edit_caption
+    else:
+        response = await message.reply_text(
+            text=_["ping_1"].format(app.mention),
+        )
+        edit_func = response.edit_text
+
     pytgping = await JARVIS.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
+
+    await edit_func(
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),
     )
